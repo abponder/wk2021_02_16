1811c0e6dfb0ee1bc4ac5b1e1f2669b8affe0313
alexbponder@gmail.com <alexbponder@gmail.com>
2021-02-16 18:35:31 -0800

    music for all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    
+    return (
+      <div>
+      <div>
+        Hello youtube
+      </div>
+
+      <iframe width="200" height="175" 
+        src="https://www.youtube.com/embed/Io2hbcrAYBw,BKLVpDTZOPQ,qr1-WpWOUk8,veHqJSC-9Lo?&loop=1&playlist=Io2hbcrAYBw,BKLVpDTZOPQ,qr1-WpWOUk8,veHqJSC-9Lo">
+      </iframe>
+
+      </div>
+     
+    );
+  }
 }
 
 export default App;
