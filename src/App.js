3b952462d1a5e38fa15a527abba173107029976a
alexbponder@gmail.com <alexbponder@gmail.com>
2021-03-18 19:38:38 -0700

    updated refresh button

--- src/App.js
+++ src/App.js
@@ -7,30 +7,52 @@ class App extends React.Component {
     super(props);
     this.state = {
       videos:[
-        {songid:'Io2hbcrAYBw', songname:'twenty one pilots: The Hype', selected:false},
-        {songid:'BKLVpDTZOPQ', songname:'Foster The People: Sit Next to Me', selected:false},
-        {songid:'qr1-WpWOUk8', songname:'lovelytheband: broken', selected:false},
-        {songid:'veHqJSC-9Lo', songname:'The XX: Intro long version', selected:false}
+        {songid:'Io2hbcrAYBw', songname:'twenty one pilots: The Hype', selected:true},
+        {songid:'BKLVpDTZOPQ', songname:'Foster The People: Sit Next to Me', selected:true},
+        {songid:'qr1-WpWOUk8', songname:'lovelytheband: broken', selected:true},
+        {songid:'veHqJSC-9Lo', songname:'The XX: Intro long version', selected:true}
       ],
-      selected:[]
+      currentselection:[
+        {songid:'Io2hbcrAYBw', songname:'twenty one pilots: The Hype', selected:true},
+        {songid:'BKLVpDTZOPQ', songname:'Foster The People: Sit Next to Me', selected:true},
+        {songid:'qr1-WpWOUk8', songname:'lovelytheband: broken', selected:true},
+        {songid:'veHqJSC-9Lo', songname:'The XX: Intro long version', selected:true}
+      ],
+      selected:['Io2hbcrAYBw','BKLVpDTZOPQ','qr1-WpWOUk8','veHqJSC-9Lo']
     }
   }
 
   handlechange = (event) => {
     console.log(event.target.name)
-    let song = this.state.videos.find((obj) => obj.songid === event.target.name )
-    song.selected = !song.selected
+    let songs = this.state.currentselection.map((obj) => {
+      if(obj.songid === event.target.name){
+        obj.selected = !obj.selected
+      }
+      return obj
+    } )
+    
     this.setState({
-      ...song
+      currentselection:songs
     })
-    console.log(this.state)
   }
 
+  onrefreshsubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      videos:this.state.currentselection,
+      selected:this.state.currentselection.filter((obj) => obj.selected).map(obj => obj.songid)
+    })
+  }
+    
   // for next time fix .map so that its logic is inthe filter method
+  //https://developers.google.com/youtube/iframe_api_reference
+  //https://www.npmjs.com/package/react-player
   render() {
-    const selected = this.state.videos.filter((obj) => obj.selected).map(obj => obj.songid)
-    console.log(selected)
-    console.log(selected.join(','))
+    
+    // const selected = this.state.videos.filter((obj) => obj.selected).map(obj => obj.songid)
+    // const selected = this.state.currentselection.filter((obj) => obj.selected).map(obj => obj.songid)
+    // console.log(selected)
+    // console.log(selected.join(','))
     return (
       <div>
       <div>
@@ -38,21 +60,26 @@ class App extends React.Component {
       </div>
 
       <iframe width="800" height="400" 
-        src={`https://www.youtube.com/embed/${selected.join(',')}?&loop=1&playlist=${selected.join(',')}`}>
+        src={`https://www.youtube.com/embed/${this.state.selected.join(',')}?&loop=1&playlist=${this.state.selected.join(',')}`}>
       </iframe>
 
-      <form>
+      <form onSubmit={this.onrefreshsubmit}>
         
-      {this.state.videos.map((video,idx) => (
+      {this.state.currentselection.map((video,idx) => (
         <label style={{display: 'block'}} onChange={this.handlechange}>
           
-        <input type="checkbox" name={video.songid} />
+        <input type="checkbox" checked={video.selected} name={video.songid} />
         {video.songname}
         </label>
       ))}
 
+      <input
+        type='submit'
+        value='refresh'
+      />
 
       </form>
+
 
       </div>
      
